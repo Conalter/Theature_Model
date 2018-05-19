22cec3d65659f94325bc0944cf50fbe15ab4ee7c
Lyle Mitchell <ljwmitchell@gmail.com>
2018-05-19 16:22:31 +0100

    adds Playwright Class with attributes

---
+++ src/main/java/models/Playwright.java
@@ -0,0 +1,13 @@
+package models;
+
+import sun.font.Script;
+
+import java.util.ArrayList;
+
+public abstract class Playwright {
+
+    private String name;
+    private int age;
+    private ArrayList<Script> scripts;
+
+}
