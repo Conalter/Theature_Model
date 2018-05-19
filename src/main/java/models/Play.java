2e79d3ffc2d06998adb5b15b507f06635e5142ae
Lyle Mitchell <ljwmitchell@gmail.com>
2018-05-19 16:31:36 +0100

    adds Theater Class with attributes

---
+++ src/main/java/models/Play.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.util.ArrayList;
+
+public abstract class Play {
+
+    private String name;
+    private String writer;
+    private ArrayList<Performer> performers;
+    private int performances;
+}
---
+++ src/main/java/models/Theater.java
@@ -0,0 +1,14 @@
+package models;
+
+import java.util.ArrayList;
+
+public abstract class Theater {
+
+    private String name;
+    private int capacity;
+    private String address;
+    private ArrayList<Showing> showings;
+
+
+
+}
